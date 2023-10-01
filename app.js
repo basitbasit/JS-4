605028738c26a582da9ae4c548084c74278e44a9
basitbasit <basitbasit4060@gmail.com>
2023-10-01 12:34:31 +0500

    JS 4

---
+++ app.js
@@ -0,0 +1,14 @@
+var MyName = "sami"
+var Age = "18 year old"
+var App = "mobile certificated operater mobile"
+var Name ="pizza\npizz\npiz\npi\np"
+var Email = "basitbasit4060@gmail.com"
+var Adress ="i am trying to learn from a book A smarter way to learn javascript"
+var Picture = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(MyName)
+alert(Age)
+alert(App)
+alert(Name)
+alert(Email)
+alert(Adress)
+alert(Picture)
